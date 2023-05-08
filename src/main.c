3ccf61536cd28a5a5e43626922e5562c1cf9a077
Michał Lower <keton22@gmail.com>
2023-05-08 21:06:22 +0200

    WiP 2

--- src/main.c
+++ src/main.c
@@ -6,66 +6,68 @@
 #include "pci.h"
 #include "rc_manager.h"
 
-#define PCI_VENDOR_NVIDIA "10de"
-#define PCI_CLASS_VGA	  "0300"
+#define PCI_CLASS_VGA  "0300"
+#define PCI_FILTER_STR "::" PCI_CLASS_VGA
 
-#define PCI_FILTER_STR	  PCI_VENDOR_NVIDIA "::" PCI_CLASS_VGA
-
-const char program_name[] = "gpu_speed";
+// ordered so faster speed == higher enum value
+typedef enum
+{
+	PCIE_SPEED_UNKNOWN = 0,
+	PCIE_SPEED_2_5_GTS,
+	PCIE_SPEED_5_GTS,
+	PCIE_SPEED_8_GTS,
+	PCIE_SPEED_16_GTS,
+	PCIE_SPEED_32_GTS,
+	PCIE_SPEED_64_GTS,
+} pcie_speed_t;
 
 void die(char *msg, ...)
 {
 	va_list args;
 
 	va_start(args, msg);
-	fprintf(stderr, "%s: ", program_name);
+	fprintf(stderr, "fatal error: ");
 	vfprintf(stderr, msg, args);
 	fputc('\n', stderr);
 	exit(1);
 }
 
-uint8_t get_conf_byte(uint8_t *data, unsigned int pos) { return data[pos]; }
+static uint8_t get_conf_byte(uint8_t *data, unsigned int pos)
+{
+	return data[pos];
+}
 
-uint16_t get_conf_word(uint8_t *data, unsigned int pos) { return data[pos] | (data[pos + 1] << 8); }
+static uint16_t get_conf_word(uint8_t *data, unsigned int pos)
+{
+	return data[pos] | (data[pos + 1] << 8);
+}
 
-uint32_t get_conf_long(uint8_t *data, unsigned int pos)
+static uint32_t get_conf_long(uint8_t *data, unsigned int pos)
 {
 	return data[pos] | (data[pos + 1] << 8) | (data[pos + 2] << 16) | (data[pos + 3] << 24);
 }
 
-static char *link_speed(int speed)
+static pcie_speed_t lnk_cap1_speed_to_enum(int link2speed)
 {
-	switch(speed) {
+	switch(link2speed) {
 	case 1:
-		return "2.5GT/s";
+		return PCIE_SPEED_2_5_GTS;
 	case 2:
-		return "5GT/s";
+		return PCIE_SPEED_5_GTS;
 	case 3:
-		return "8GT/s";
+		return PCIE_SPEED_8_GTS;
 	case 4:
-		return "16GT/s";
+		return PCIE_SPEED_16_GTS;
 	case 5:
-		return "32GT/s";
+		return PCIE_SPEED_32_GTS;
 	case 6:
-		return "64GT/s";
+		return PCIE_SPEED_64_GTS;
 	default:
-		return "unknown";
+		return PCIE_SPEED_UNKNOWN;
 	}
 }
 
-static char *link_compare(int type, int sta, int cap)
-{
-	if(sta > cap)
-		return " (overdriven)";
-	if(sta == cap)
-		return "";
-	if((type == PCI_EXP_TYPE_ROOT_PORT) || (type == PCI_EXP_TYPE_DOWNSTREAM)
-	   || (type == PCI_EXP_TYPE_PCIE_BRIDGE))
-		return "";
-	return " (downgraded)";
-}
-
-static const char *cap_express_link2_speed_cap(int vector)
+static pcie_speed_t lnk_cap2_speed_to_enum(int vector)
 {
 	/*
 	 * Per PCIe r5.0, sec 8.2.1, a device must support 2.5GT/s and is not
@@ -73,39 +75,60 @@ static const char *cap_express_link2_speed_cap(int vector)
 	 * highest supported rate.
 	 */
 	if(vector & 0x60)
-		return "RsvdP";
+		return PCIE_SPEED_64_GTS;
 	if(vector & 0x10)
-		return "2.5-32GT/s";
+		return PCIE_SPEED_32_GTS;
 	if(vector & 0x08)
-		return "2.5-16GT/s";
+		return PCIE_SPEED_16_GTS;
 	if(vector & 0x04)
-		return "2.5-8GT/s";
+		return PCIE_SPEED_8_GTS;
 	if(vector & 0x02)
-		return "2.5-5GT/s";
+		return PCIE_SPEED_5_GTS;
 	if(vector & 0x01)
-		return "2.5GT/s";
+		return PCIE_SPEED_2_5_GTS;
 
-	return "Unknown";
+	return PCIE_SPEED_UNKNOWN;
 }
 
-static const char *cap_express_link2_speed(int type)
+static pcie_speed_t lnk_ctl2_speed_to_enum(int type)
 {
 	switch(type) {
 	case 0: /* hardwire to 0 means only the 2.5GT/s is supported */
 	case 1:
-		return "2.5GT/s";
+		return PCIE_SPEED_2_5_GTS;
 	case 2:
-		return "5GT/s";
+		return PCIE_SPEED_5_GTS;
 	case 3:
-		return "8GT/s";
+		return PCIE_SPEED_8_GTS;
 	case 4:
-		return "16GT/s";
+		return PCIE_SPEED_16_GTS;
 	case 5:
-		return "32GT/s";
+		return PCIE_SPEED_32_GTS;
 	case 6:
-		return "64GT/s";
+		return PCIE_SPEED_64_GTS;
 	default:
-		return "Unknown";
+		return PCIE_SPEED_UNKNOWN;
+	}
+}
+
+static char *pcie_speed_to_str(pcie_speed_t speed)
+{
+	switch(speed) {
+	case PCIE_SPEED_2_5_GTS:
+		return "2.5GT/s";
+	case PCIE_SPEED_5_GTS:
+		return "5GT/s";
+	case PCIE_SPEED_8_GTS:
+		return "8GT/s";
+	case PCIE_SPEED_16_GTS:
+		return "16GT/s";
+	case PCIE_SPEED_32_GTS:
+		return "32GT/s";
+	case PCIE_SPEED_64_GTS:
+		return "64GT/s";
+	case PCIE_SPEED_UNKNOWN:
+	default:
+		return "unknown";
 	}
 }
 
@@ -116,7 +139,7 @@ int main(void)
 		die("rc_manager_init");
 	}
 
-	struct pci_filter filter = { 0 };
+	struct pci_filter filter = {0};
 	struct pci_access *pacc = pci_alloc();
 	if(!pacc) {
 		die("pci_alloc");
@@ -131,11 +154,10 @@ int main(void)
 	pci_scan_bus(pacc);
 
 	for(struct pci_dev *dev = pacc->devices; dev; dev = dev->next) {
-
 		if(!pci_filter_match(&filter, dev)) {
 			continue;
 		}
-		char devbuf[256] = { 0 };
+		char devbuf[256] = {0};
 
 		pci_fill_info(dev, PCI_FILL_IDENT | PCI_FILL_LABEL | PCI_FILL_CAPS | PCI_FILL_EXT_CAPS);
 
@@ -149,37 +171,45 @@ int main(void)
 			continue;
 		}
 
-		uint8_t cap_express_data[58] = { 0 };
+		// per PCIe spec 'devices with links' have first 56 bytes of PCI Express capability register
+		// populated
+		uint8_t cap_express_data[56] = {0};
 
 		// get PCIe config data
-		if(!dev->no_config_access
-		   && !pci_read_block(dev, cap->addr, cap_express_data, sizeof(cap_express_data))) {
+		if(!dev->no_config_access &&
+		   !pci_read_block(dev, cap->addr, cap_express_data, sizeof(cap_express_data))) {
 			printf("Warning: no_config_access\n");
 			continue;
 		}
 
 		uint32_t lnk_cap_raw = get_conf_long(cap_express_data, PCI_EXP_LNKCAP);
-		uint32_t lnk_cap_speed = lnk_cap_raw & PCI_EXP_LNKCAP_SPEED;
+		pcie_speed_t lnk_cap_speed = lnk_cap1_speed_to_enum(lnk_cap_raw & PCI_EXP_LNKCAP_SPEED);
+
 		uint32_t lnk_cap_width = (lnk_cap_raw & PCI_EXP_LNKCAP_WIDTH) >> 4;
 
-		printf("\tLnkCap:\tSpeed %s, Width x%d\n", link_speed(lnk_cap_speed), lnk_cap_width);
+		printf("\tLnkCap:\tMaximum Link Speed %s, Maximum Link Width x%d\n",
+			   pcie_speed_to_str(lnk_cap_speed), lnk_cap_width);
 
 		uint16_t lnk_sta_raw = get_conf_word(cap_express_data, PCI_EXP_LNKSTA);
-		uint32_t lnk_sta_speed = lnk_sta_raw & PCI_EXP_LNKSTA_SPEED;
+		pcie_speed_t lnk_sta_speed = lnk_cap1_speed_to_enum(lnk_sta_raw & PCI_EXP_LNKSTA_SPEED);
+
 		uint32_t lnk_sta_width = (lnk_sta_raw & PCI_EXP_LNKSTA_WIDTH) >> 4;
-		printf("\tLnkSta:\tSpeed %s%s, Width x%d%s\n", link_speed(lnk_sta_speed),
-			   link_compare(PCI_EXP_TYPE_LEG_END, lnk_sta_speed, lnk_cap_speed), lnk_sta_width,
-			   link_compare(PCI_EXP_TYPE_LEG_END, lnk_sta_width, lnk_cap_width));
+
+		printf("\tLnkSta:\tNegotiated Link Speed %s, Negotiated Link Width x%d\n",
+			   pcie_speed_to_str(lnk_sta_speed), lnk_sta_width);
 
 		uint32_t lnk_cap2_raw = get_conf_long(cap_express_data, PCI_EXP_LNKCAP2);
-		if(lnk_cap2_raw) {
-			printf("\tLnkCap2: Supported Link Speeds: %s\n",
-				   cap_express_link2_speed_cap(PCI_EXP_LNKCAP2_SPEED(lnk_cap2_raw)));
+		pcie_speed_t lnk_cap2_speed = lnk_cap2_speed_to_enum(PCI_EXP_LNKCAP2_SPEED(lnk_cap2_raw));
+
+		if(lnk_cap2_speed != PCIE_SPEED_UNKNOWN) {
+			printf("\tLnkCap2: Supported Link Speed: 2.5GT/s - %s\n",
+				   pcie_speed_to_str(lnk_cap2_speed));
 		}
 
 		uint16_t lnk_ctl2_raw = get_conf_word(cap_express_data, PCI_EXP_LNKCTL2);
-		printf("\tLnkCtl2: Target Link Speed: %s\n",
-			   cap_express_link2_speed(PCI_EXP_LNKCTL2_SPEED(lnk_ctl2_raw)));
+		pcie_speed_t lnk_ctl2_speed = lnk_ctl2_speed_to_enum(PCI_EXP_LNKCTL2_SPEED(lnk_ctl2_raw));
+
+		printf("\tLnkCtl2: Target Link Speed: %s\n", pcie_speed_to_str(lnk_ctl2_speed));
 	}
 
 	pci_cleanup(pacc);
